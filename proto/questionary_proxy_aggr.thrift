2c2a6ece5210ddbd8e1e1e64a7113722b49c8d68
vbanin <vitaxa93gamebox@gmal.com>
2019-05-24 09:56:48 +0300

    DaData initial commit

--- proto/questionary_proxy_aggr.thrift
+++ proto/questionary_proxy_aggr.thrift
@@ -2,15 +2,23 @@ namespace java com.rbkmoney.damsel.questionary_proxy_aggr
 namespace erlang questionary_proxy_aggr
 
 include "kontur_focus_api.thrift"
+include "dadata_api.thrift"
 
 exception KonturFocusRequestException {}
 
-service QuestionaryProxyAggrHandler {
+exception DaDataRequestException {}
+
+service QuestionaryAggrProxyHandler {
 
     kontur_focus_api.KonturFocusResponse RequestKonturFocus(
                 1: kontur_focus_api.KonturFocusRequest request,
                 2: kontur_focus_api.KonturFocusEndPoint endPoint
             ) throws (1: KonturFocusRequestException ex)
+
+    dadata_api.DaDataResponse RequestDaData(
+                1: dadata_api.DaDataRequest request,
+                2: dadata_api.DaDataEndpoint endPoint
+            ) throws (1: DaDataRequestException ex)
 
 }
 
